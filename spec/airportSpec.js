bdf4808472ebe85167dabfe5c311fd66194fb3eb
domw30 <domw30@gmail.com>
2019-05-01 14:57:14 +0100

    planes can land user story complete

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -1,13 +1,17 @@
-'use strict'
+'use strict';
 
 describe('Airport', function(){
   var airport;
-
-  beforeEach(function()){
+  var plane;
+  beforeEach(function(){
     airport = new Airport();
+    plane = jasmine.createSpy('plane',['land']);
   });
-
   it('has no planes by default', function(){
     expect(airport.planes()).toEqual([]);
+  });
+  it('can clear planes for landing', function(){
+    airport.clearForLanding(plane);
+    expect(airport.planes()).toEqual([plane]);
   });
 });
--- src/Plane.js
+++ src/Plane.js
@@ -1,4 +1,5 @@
-'use strict';
-
 function Plane(){}
-Plane.prototype.land = function (){};
+
+Plane.prototype.land = function(airport){
+  airport.clearForLanding(this);
+};
