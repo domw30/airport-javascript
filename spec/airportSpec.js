785d0daeb4a38d363b2177de16294523b10f75f9
domw30 <domw30@gmail.com>
2019-05-01 16:20:55 +0100

    add stormy weather restriction, user story 3 complete

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -14,4 +14,19 @@ describe('Airport', function(){
     airport.clearForLanding(plane);
     expect(airport.planes()).toEqual([plane]);
   });
+  it('can clear planes for take off', function(){
+    airport.clearForLanding(plane);
+    airport.clearForTakeOff(plane);
+    expect(airport.planes()).toEqual([]);
+  });
+  it('can check for stormy weather conditions', function(){
+    expect(airport.isStormy()).toBeFalsy();
+  });
+
+  describe('when the weather is stormy', function(){
+    it('does not allow planes for take off', function(){
+      spyOn(airport,'isStormy').and.returnValue(true);
+      expect(function(){ airport.clearForTakeOff(plane); }).toThrowError('cannot takeoff during storm');
+    });
+  });
 });
